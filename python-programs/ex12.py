a07ff810d8994c788e6cd6ece9a1568c80a126dd
I832861 <ganesh.nandamuru@sap.com>
2022-08-25 20:42:37 -0400

    del folders

--- python-programs/ex12.py
+++
@@ -1,7 +0,0 @@
-# Prompting People
-
-age = input("How old are you? ")
-height = input("How tall are you? ")
-weight = input("How much do you weight? ")
-
-print(f"So, you're {age} old, {height} tall and {weight} heavy.")
